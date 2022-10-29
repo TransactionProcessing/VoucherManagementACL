1a2021f69263803043138307cc2b35c0b545fca6
Stuart Ferguson <stuart_ferguson1@btinternet.com>
2022-10-29 11:46:41 +0100

    Update docker helper and health output

--- VoucherManagementACL/Startup.cs
+++ VoucherManagementACL/Startup.cs
@@ -114,7 +114,7 @@ namespace VoucherManagementACL
                                  endpoints.MapHealthChecks("health", new HealthCheckOptions()
                                                                      {
                                                                          Predicate = _ => true,
-                                                                         ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                                                                         ResponseWriter = Shared.HealthChecks.HealthCheckMiddleware.WriteResponse
                                                                      });
                              });
 
